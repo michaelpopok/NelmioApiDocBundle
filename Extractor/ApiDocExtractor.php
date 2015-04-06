26596de4b726c73c44cbf008f50b591b7286237f
Mykhailo Popok <mipo@ciklum.com>
2015-04-06 16:02:40 +0300

    fixed issue #565 - Required form fields for PUT method always false?

--- Extractor/ApiDocExtractor.php
+++ Extractor/ApiDocExtractor.php
@@ -306,13 +306,6 @@ class ApiDocExtractor
             $parameters = $this->clearClasses($parameters);
             $parameters = $this->generateHumanReadableTypes($parameters);
 
-            if ('PUT' === $annotation->getMethod()) {
-                // All parameters are optional with PUT (update)
-                array_walk($parameters, function ($val, $key) use (&$parameters) {
-                    $parameters[$key]['required'] = false;
-                });
-            }
-
             $annotation->setParameters($parameters);
         }
 
